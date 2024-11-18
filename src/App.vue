9ab25fed82c1d5f82f89f7c71fa9eeb1ef472495
PierceLem <Parcwill@gmail.com>
2024-11-18 11:36:52 -0500

    config file errors
    Previous error that was solved by wrapping templates in div tags to create one element is now solved by fixing the .eslintrc.js file which also solves some other errors appearing under the "module" perameter in the babel config file.

--- src/App.vue
+++ src/App.vue
@@ -13,30 +13,28 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 -->
 <template>
-  <div>
-    <sidenav
-      :custom_class="color"
-      :class="[isRTL ? 'fixed-end' : 'fixed-start']"
-      v-if="showSidenav"
+  <sidenav
+    :custom_class="color"
+    :class="[isRTL ? 'fixed-end' : 'fixed-start']"
+    v-if="showSidenav"
+  />
+  <main
+    class="main-content position-relative max-height-vh-100 h-100 overflow-x-hidden"
+  >
+    <!-- nav -->
+    <navbar
+      :class="[isNavFixed ? navbarFixed : '', isAbsolute ? absolute : '']"
+      :color="isAbsolute ? 'text-white opacity-8' : ''"
+      :minNav="navbarMinimize"
+      v-if="showNavbar"
     />
-    <main
-      class="main-content position-relative max-height-vh-100 h-100 overflow-x-hidden"
-    >
-      <!-- nav -->
-      <navbar
-        :class="[isNavFixed ? navbarFixed : '', isAbsolute ? absolute : '']"
-        :color="isAbsolute ? 'text-white opacity-8' : ''"
-        :minNav="navbarMinimize"
-        v-if="showNavbar"
-      />
-      <router-view />
-      <app-footer v-show="showFooter" />
-      <configurator
-        :toggle="toggleConfigurator"
-        :class="[showConfig ? 'show' : '', hideConfigButton ? 'd-none' : '']"
-      />
-    </main>
-  </div>
+    <router-view />
+    <app-footer v-show="showFooter" />
+    <configurator
+      :toggle="toggleConfigurator"
+      :class="[showConfig ? 'show' : '', hideConfigButton ? 'd-none' : '']"
+    />
+  </main>
 </template>
 <script>
 import Sidenav from "./examples/Sidenav";
